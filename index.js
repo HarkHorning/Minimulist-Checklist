04574568bc713455f0b451818ed077904fe42872
HarkHorning <147337852+HarkHorning@users.noreply.github.com>
2023-12-12 18:40:32 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const list = document.getElementById("list");
+const enterButton = document.getElementById("enterButton");
+const textBoxObj = document.getElementById("textBox")
+
+const addFunk = () => {
+    if (textBoxObj.value != "") {
+        
+    let newItem = document.createElement('li');
+    newItem.textContent = textBoxObj.value;
+    list.appendChild(newItem);
+    textBoxObj.value = "";
+
+    newItem.addEventListener('dblclick', () => {
+        list.removeChild(newItem);
+    });
+
+    } else {
+        alert("Please enter a task. Nerd.");
+    }
+}
+
+enterButton.addEventListener('click', addFunk);
+
+textBoxObj.addEventListener('keypress', (e) => {
+    if (e.key === "Enter") {
+        addFunk()
+    }
+});
